8a50fea0bf15dec25026e982bbf34ce175673451
Ken Leland III <kenleezle@gmail.com>
2014-10-12 03:24:49 -0400

    s/floppyforms/bootstrapforms/

--- charitme/settings.py
+++ charitme/settings.py
@@ -130,7 +130,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'charities',
-    'floppyforms'
+    'bootstrap3'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
